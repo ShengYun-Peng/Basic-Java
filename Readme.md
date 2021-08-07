6f4e175a0b31fc58b346ba2d427c081f2d7cc1a1
Sheng-Yun Peng <anthony.peng.2021@gmail.com>
2021-08-07 16:28:14 -0400

    Add readme

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Basic Java Programming
+Here is a playground to learn and test some basic java programming techniques.  
+I follow the book *Introduction to Java programming and data structures*
+
+-[x] Chapter 1
+-[x] Chapter 2
+-[x] Chapter 3
+-[ ] Chapter 4
+-[ ] Chapter 5
+-[ ] Chapter 6
+-[ ] Chapter 7
+-[ ] Chapter 8
+-[ ] Chapter 9
+-[ ] Chapter 10
+
--- src/chap4/ComputeAngle.java
+++ src/chap4/ComputeAngle.java
@@ -1,15 +1,35 @@
 package chap4;
 
+import java.util.Scanner;
+
 public class ComputeAngle {
-    public double computeLength(double x1, double x2, double y1, double y2) {
+    public static double computeLength(double x1, double x2, double y1, double y2) {
         return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
     }
 
-    public double computeAngle(double a, double b, double c) {
+    public static double computeAngle(double a, double b, double c) {
         return Math.acos((a * a - b * b - c * c) / (-2 * b * c));
     }
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter (x1, y1), (x2, y2), (x3, y3): ");
 
+        double x1 = input.nextDouble();
+        double y1 = input.nextDouble();
+        double x2 = input.nextDouble();
+        double y2 = input.nextDouble();
+        double x3 = input.nextDouble();
+        double y3 = input.nextDouble();
+
+        double a = computeLength(x2, x3, y2, y3);
+        double b = computeLength(x1, x3, y1, y3);
+        double c = computeLength(x1, x2, y1, y2);
+
+        double angleA = Math.toDegrees(computeAngle(a, b, c));
+        double angleB = Math.toDegrees(computeAngle(b, a, c));
+        double angleC = Math.toDegrees(computeAngle(c, b, a));
+
+        System.out.println("Angle A: " + angleA + ", Angle B: " + angleB + ", Angle C: " + angleC);
     }
 }
